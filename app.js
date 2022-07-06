9171d65a1f77d148e8bf834d1ba3cfde3b207018
Gautam <kumawatgautam305@gmail.com>
2022-07-06 22:43:15 +0530

    Added a Procfile

--- app.js
+++ app.js
@@ -234,8 +234,12 @@ app.post("/Userdb",function(req,res)
     });
 });
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000,function(req,res)
+app.listen(port,function(req,res)
 {
     console.log("My HealthDesk Server started...")
 });
